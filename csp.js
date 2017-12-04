cac207495c4f29a998205bf7ac780fc3575f66ff
Your Name <gabehab@hotmail.com>
2017-12-04 16:54:15 -0500

    rating

--- csp.js
+++ csp.js
@@ -10,6 +10,8 @@ function Square(variable, ind, row, col, neighbors, constraints) {
   this.neighbors = neighbors;
 }
 
+
+let clone;
 let MAX = 100; //set the width of the square board(the MAX value)
 window.arr = []; //arr is the BOARD of squares
 let c = 0; //init counter var;
@@ -40,9 +42,9 @@ arr.forEach(o => { //generate constraints for each square of the board
   }
 });
 
-arr.forEach(sq => { //remove the square if its constraints contain itself
-  if (sq.constraints.indexOf(sq) >= 0) sq.constraints.splice(sq.constraints.indexOf(sq), 1)
-})
+// arr.forEach(sq => { //remove the square if its constraints contain itself
+//   if (sq.constraints.indexOf(sq) >= 0) sq.constraints.splice(sq.constraints.indexOf(sq), 1)
+// })
 
 arr.forEach(sq => { //remove dups in constraints
   sq.constraints.forEach(x => {
@@ -50,128 +52,109 @@ arr.forEach(sq => { //remove dups in constraints
   });
 });
 
-
+let gdata = [];
 let conx = true;
 let itt = 0;
 // var ts;
-
+let initialsq = [];
 //int MIN-CONFLICTS
-for (var ix = 0; ix < MAX; ix++) {
-  //set up initial board
-  let column = arr.filter(sqx => sqx.col === ix); //get current col to iterate
-  // let sq = column[Math.floor(Math.random() * column.length)];
-  let q = findBest(column); //fidn the best init spot for queen
-  // let q = column[Math.floor(Math.random() * column.length)];
-  // console.log("INIT", q.ind);
-  q.variable = 1;
-  arr.forEach(s => { //update boards conflicts each time
-    s.flicts = updateCon(s);
-  });
+function init() {
+  for (var ix = 0; ix < MAX; ix++) {
+    //set up initial board
+    let column = arr.filter(sqx => sqx.col === ix); //get current col to iterate
+    // let sq = column[Math.floor(Math.random() * column.length)];
+    let q = findBest(column); //fidn the best init spot for queen
+    // let q = column[Math.floor(Math.random() * column.length)];
+    // console.log("INIT", q.ind);
+    q.variable = 1;
+    initialsq.push(q.ind);
+    arr.forEach(s => { //update boards conflicts each time
+      s.flicts = updateCon(s);
+    });
+  }
 }
+let qd = [];
+let s1 = [];
 
-while (conx) {
-  let old = 0;
-  let nold = 0;
-  let list;
-  let badSq;
-  let newSq;
-  let column;
+function main() {
+  while (conx) {
+    let old = 0;
+    let nold = 0;
+    let list;
+    let badSq;
+    let newSq;
+    let column;
+    let q = 0;
 
-  if (fin() === MAX) break; //if solved, break loop
+    if (fin() === MAX) break; //if solved, break loop
 
-  badSq = findQueens(arr); //find all queens
-  badSq = findWorst(badSq); //find the highest conflict queens
-  column = arr.filter(sqx => (sqx.col === badSq.col && sqx.ind !== badSq.ind)); //find all sq in bad queen col
-  arr.forEach(s => {
-    s.flicts = updateCon(s);
-    old += s.flicts;
-  });
-  newSq = findBest(column);
+    badSq = findQueens(arr); //find all queens
+    badSq = findWorst(badSq); //find the highest conflict queens
+    column = arr.filter(sqx => (sqx.col === badSq.col && sqx.ind !== badSq.ind)); //find all sq in bad queen col
+    arr.forEach(s => { //update all squares conflicts
+      s.flicts = updateCon(s);
+      if (s.ind !== badSq.ind) old += parseInt(s.flicts); //don't incldue self when comparing values
+    });
+    //stats only (not appart of algo)
+    column.forEach(s => q += s.flicts);
+    qd.push(q);
+    gdata.push(old);
+    ///
 
-  newSq.variable = 1;
-  badSq.variable = 0;
-  // console.log(newSq.variable, badSq.variable);
+    newSq = findBest(column);
 
-  arr.forEach(s => {
-    s.flicts = updateCon(s);
-    nold += s.flicts;
-  });
+    newSq.variable = 1;
+    badSq.variable = 0;
+    // console.log(newSq.variable, badSq.variable);
 
-  // console.log("test", old, nold);
-  if (old < nold) {
-    console.log('old less cur');
-    if (badSq.flicts <= newSq.flicts) {
+    arr.forEach(s => { //update with new variable for new square
+      s.flicts = updateCon(s);
+      if (s.ind !== newSq.ind) nold += parseInt(s.flicts);
+    });
+
+    if (newSq.flicts > badSq.flicts) { //check if new is better than old sq
       newSq.variable = 0;
       badSq.variable = 1;
-    }
-  } else if (old === nold) {
-    if (badSq.flicts < newSq.flicts) {
-      newSq.variable = 0;
-      badSq.variable = 1;
-    } else if (badSq.flicts === newSq.flicts) {
-      if (Math.floor(Math.random() * 2) === 0) {
+      s1.push(badSq.flicts); //stats only (not appart of algo)
+    } else if (newSq.flicts === badSq.flicts) { //theyre the same
+      if (Math.floor(Math.random() * 2) === 0) { //50/50 coin flip to change back or not if same flicts
+        s1.push(badSq.flicts);
         newSq.variable = 0;
         badSq.variable = 1;
+      } else {
+        s1.push(newSq.flicts); //stats only (not appart of algo)
       }
-    } else if (badSq.flicts > newSq.flicts) {
-      newSq.variable = 1;
-      badSq.variable = 0;
+    } else {
+      s1.push(newSq.flicts); //stats only (not appart of algo)
     }
+
+    itt++; //max iterations
+    if (itt === 100) conx = false;
+    // printBoard();
   }
 
-  arr.forEach(s => {
+  arr.forEach(s => { //update conflicts
     s.flicts = updateCon(s);
   });
 
-  itt++;
-  if (itt === 50) conx = false;
-
-  // printBoard();
+  console.log("MIN-CONFLICTS completed");
+  console.log("Steps: ", itt, "algo found solutioned : ", fin() === MAX);
 }
-arr.forEach(s => {
-  s.flicts = updateCon(s);
-  // nold += s.flicts;
-});
+
+console.time("Time taken");
+init();
+main();
+console.timeEnd("Time taken");
 
 
 
-// arr.forEach(s => {
-// for(let i = 0; i < MAX; i++){
-//   // s.flicts = updateCon(s);
-//
-//   var column = arr.filter(q=> i === q.col);
-//
-//   var cur = column.filter(q=> (q.variable === 1));
-//   cur = cur[0];
-//   console.log(cur, cur.ind);
-//   // cur = arr.find(s=> s.ind === cur.ind);
-//   var best = findBest(column);
-//   console.log("best", best.col, best.row, "current q", cur.ind);
-//   if(best.flicts <= cur.flicts){
-//     best.variable = 1;
-//     cur.variable = 0;
-//   }
-//
-// };
 
-
-
-arr.forEach(s => {
-  s.flicts = updateCon(s);
-  // nold += s.flicts;
-});
-
-printBoard();
-// }
-
-
-
-function fin() {
+function fin() { //check if squares are all solved
   return arr.filter(s => s.variable === 1 && s.flicts === 0)
     .length;
 }
 
-function printBoard() {
+function printBoard() { //print html board
 
   let asd = document.querySelector("#d");
   asd.innerHTML = "";
@@ -186,7 +169,7 @@ function printBoard() {
   })
 }
 
-function findWorst(arrx) {
+function findWorst(arrx) { //finds square with highest conflict
   var smallest = arrx[0];
   var temp = [];
   for (let i = 1; i < arrx.length; i++) {
@@ -198,15 +181,15 @@ function findWorst(arrx) {
     }
   }
   if (temp.length >= 1) {
-    if (smallest.flicts <= temp[0].flicts) {
+    if (smallest.flicts <= temp[0].flicts) { //if multiple tied squares
       temp.push(smallest);
-      return temp[Math.floor(Math.random() * temp.length)];
+      return temp[Math.floor(Math.random() * temp.length)]; //return a random one
     }
   }
   return smallest;
 }
 
-function findBest(arrx) {
+function findBest(arrx) { //find square with least conflicts
   var smallest = arrx[0];
   var temp = [];
   for (let i = 1; i < arrx.length; i++) {
@@ -238,4 +221,78 @@ function findQueens(arr) {
   return arr.filter(s => s.variable === 1);
 }
 
-console.dir(arr);
+
+
+// END OF ALGORITHM
+
+let lb = Array(gdata.length)
+  .fill()
+  .map((e, i) => i + 1);
+
+var data = {
+  // A labels array that can contain any sort of values
+  labels: lb,
+  // Our series array that contains series objects or in this case series data arrays
+  series: [
+    gdata
+  ]
+};
+
+// Create a new line chart object where as first parameter we pass in a selector
+// that is resolving to our chart container element. The Second parameter
+// is the actual data object.
+var options = {
+  width: 500,
+  height: 500,
+  showPoint: false,
+  axisY: {
+    onlyInteger: true,
+    //  offset: 20
+  },
+  axisX: {
+    onlyInteger: true,
+  }
+};
+new Chartist.Line('#graph', data, options);
+new Chartist.Line('#qcon', {
+  // A labels array that can contain any sort of values
+  labels: lb,
+  // Our series array that contains series objects or in this case series data arrays
+  series: [
+    qd
+  ]
+}, options);
+new Chartist.Line('#qs', {
+  // A labels array that can contain any sort of values
+  labels: lb,
+  // Our series array that contains series objects or in this case series data arrays
+  series: [
+    s1
+  ]
+}, options);
+
+// console.dir(arr);
+// printBoard();
+function download() {
+  let rowArray;
+  let csvContent = "data:text/csv;charset=utf-8,";
+  // arr.forEach(function(rowArray){
+  for (let x = 0; x < MAX; x++) {
+    rowArray = arr.filter(s => s.col === x)
+      .map(s => {
+        if (s.variable === 1) return "Q";
+        return s.flicts
+      });
+    //  let row = ;
+    csvContent += rowArray.join(",") + "\r\n"; // add carriage return
+  }
+
+
+  var encodedUri = encodeURI(csvContent);
+  var link = document.createElement("a");
+  link.setAttribute("href", encodedUri);
+  link.setAttribute("download", MAX + "-queens.csv");
+  document.body.appendChild(link); // Required for FF
+
+  link.click();
+}
